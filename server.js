271eb39a22b1e7602931febb4d326c8db8d3e888
prezvious <mezmaids@gmail.com>
2026-08-23 01:41:58 +0800

    Fix Vercel routing and static directory resolution for public assets

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const {
     isSupabaseConfigured,
     findEmailByUsername,
@@ -25,7 +26,11 @@ const { FactionEngine, getMultiplierTable } = require('./src/engine/factionEngin
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '5mb' }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const DEFAULT_STATE = {
     cash: 0,
